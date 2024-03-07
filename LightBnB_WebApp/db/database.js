e9b57699630c0b893aab273d06d5dfd54eea4160
v-lane <victoria.lane.m@gmail.com>
2024-03-07 09:49:30 -0500

    Refactor database query getAllReservatiosn

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -74,9 +74,28 @@ const addUser = (user) => {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-};
+// const getAllReservations = function (guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
+const getAllReservations = (guest_id, limit = 10) => {
+
+  return pool
+    .query(`SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating
+    FROM reservations
+      FULL OUTER JOIN property_reviews ON reservations.id = property_reviews.reservation_id
+      FULL OUTER JOIN properties ON properties.id = reservations.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      return(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+}
+
 
 /// Properties
 
