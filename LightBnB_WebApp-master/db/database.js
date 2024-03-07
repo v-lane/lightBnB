ddb63bf37a4cdc13405568941236bc88be53330d
v-lane <victoria.lane.m@gmail.com>
2024-03-06 20:49:57 -0500

    Add link from server to database, refactor allProperties query

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,6 +1,15 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
 /// Users
 
 /**
@@ -59,12 +68,25 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+// const getAllProperties = function (options, limit = 10) {
+//   const limitedProperties = {};
+//   for (let i = 1; i <= limit; i++) {
+//     limitedProperties[i] = properties[i];
+//   }
+//   return Promise.resolve(limitedProperties);
+// };
+
+const getAllProperties = (options, limit = 10) => {
+
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`,[limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.mesage);
+    });
 };
 
 /**
